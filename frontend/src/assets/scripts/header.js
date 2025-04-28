30692f76d86948af2e520c419d4e98eafe03033a
Victor Hugo Fedatto <victorhugofedatto@gmail.com>
2025-04-28 14:34:41 -0300

    Update Responsividade Header
    Algumas alterações para permitir a melhor visualização dos usuários em diversos dispositivos.

---
+++ frontend/src/assets/scripts/header.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger');
+const menuLateral = document.getElementById('menu-lateral');
+const overlay = document.getElementById('overlay');
+      
+hamburger.addEventListener('click', () => {
+    menuLateral.style.left = '0';
+    overlay.style.display = 'block';
+});
+      
+overlay.addEventListener('click', () => {
+    menuLateral.style.left = '-250px';
+    overlay.style.display = 'none';
+});
