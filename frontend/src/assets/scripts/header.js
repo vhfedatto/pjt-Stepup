18728471bd77e2e0928556beeafd71ba6b213634
Victor Hugo Fedatto <victorhugofedatto@gmail.com>
2025-04-29 14:55:56 -0300

    Update Header, Footer
    Apenas a remoção do bug do menu duplicado e a alteração do tamanho da imagem do footer.

--- frontend/src/assets/scripts/header.js
+++ frontend/src/assets/scripts/header.js
@@ -1,10 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
 const menuLateral = document.getElementById('menu-lateral');
 const overlay = document.getElementById('overlay');
-      
+
 hamburger.addEventListener('click', () => {
     menuLateral.style.left = '0';
     overlay.style.display = 'block';
+    overlay.style.remove.display = 'none';
 });
       
 overlay.addEventListener('click', () => {
